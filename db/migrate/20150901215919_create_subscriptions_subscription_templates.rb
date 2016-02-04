2818b075e3e833a5287b9008de53c46c01bdaa00
Ben McFadden <ben@forgeapps.com>
2016-02-04 12:33:37 -0700

    So those migration that were broken.. weren't broken in the way I thought they were. Now they're fixed properly.

--- db/migrate/20150901215919_create_subscriptions_subscription_templates.rb
+++ db/migrate/20150901215919_create_subscriptions_subscription_templates.rb
@@ -6,12 +6,12 @@ class CreateSubscriptionsSubscriptionTemplates < ActiveRecord::Migration
       t.integer :interval
       t.string :slug
       t.boolean :visible
-      t.references :subscriptions_subscription_template_group, foreign_key: true
+      t.references :subscription_template_group
       t.integer :position
 
       t.timestamps null: false
     end
-    add_index :subscriptions_subscription_templates, :subscriptions_subscription_template_group_id, name: "index_subscriptions_subscription_template_group_id"
+    add_index :subscriptions_subscription_templates, :subscription_template_group_id, name: "index_subscriptions_subscription_template_group_id"
     add_index :subscriptions_subscription_templates, :name
     add_index :subscriptions_subscription_templates, :amount_cents
     add_index :subscriptions_subscription_templates, :interval
