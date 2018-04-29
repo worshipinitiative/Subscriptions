69fdd14971116d103ef8436cec75af135c0c770e
Ryan Pitts <ryanpitts1@gmail.com>
2018-04-29 00:27:13 -0500

    Changing trial amount days.

--- lib/subscriptions/concerns/models/subscription.rb
+++ lib/subscriptions/concerns/models/subscription.rb
@@ -1,24 +1,24 @@
 module Subscriptions
   module Concerns
-    module Models      
+    module Models
       module Subscription
         extend ActiveSupport::Concern
 
         included do
           include DateTimeScopeable
-          
+
           belongs_to :ownerable, polymorphic: true, touch: true
           has_many :subscription_periods, class_name: "Subscriptions::SubscriptionPeriod"
-    
+
           # NOTE: IF YOU ADD AN INTERVAL YOU MUST HANDLE IT IN THE CYCLE SUBSCRIPTION BILLING PERIOD METHOD AND ADD IT TO THE SUBSCRIPTION TEMPLATE MODEL!!!
-    
+
           enum interval: { year: 0, six_month: 1, three_month: 2, month: 3 }
-    
+
           enum status: {good_standing: 0, suspended: 1, cancelled: 2, cancel_at_end: 4, suspended_payment_failed: 5, trialing: 6, trial_expired: 7, cancelled_payment_failed: 8}
 
           scope :cycleable, ->{ where.not(status: [self.statuses[:suspended], self.statuses[:suspended_payment_failed], self.statuses[:cancelled], self.statuses[:cancelled_payment_failed], self.statuses[:trial_expired]]) }
           scope :ready_to_cycle, ->{ cycleable.where( "next_bill_date < ?", Time.now ) }
-          
+
           validates :amount_cents_base,          numericality: { greater_than_or_equal_to: 0 }
           validates :amount_cents_next_period,   numericality: { greater_than_or_equal_to: 0 }
           validate :uniqueness_of_ownerable, on: :create
@@ -27,10 +27,10 @@ module Subscriptions
           before_create :set_initial_amount_cents
           after_create :create_open_invoice
           before_save :update_current_status_at
-          
-          @@trial_days = 7
+
+          @@trial_days = 30
           cattr_accessor :trial_days
-          
+
           # On selecting a template this field map will be used
           # to map subscription fields to subscription template fields
           @@subscription_template_field_map = {
@@ -39,7 +39,7 @@ module Subscriptions
             interval: :interval
           }
           cattr_accessor :subscription_template_field_map
-          
+
           def cancel_at_end!
             raise "Cannot cancel at end. Status not in good standing!" unless good_standing? || trialing?
             self.update_attributes(
@@ -57,35 +57,35 @@ module Subscriptions
             )
             status_reinstated
           end
-        
+
           def good_standing!
             self.update_attributes(
               status: :good_standing,
             )
             status_changed_to_good_standing
           end
-        
+
           def suspended!
             self.update_attributes(
               status: :suspended,
             )
             status_changed_to_suspended
           end
-        
+
           def cancelled!
             self.update_attributes(
               status: :cancelled,
             )
             status_changed_to_cancelled
           end
-        
+
           def suspended_payment_failed!
             self.update_attributes(
               status: :suspended_payment_failed,
             )
             status_changed_to_suspended_payment_failed
           end
-          
+
           def cancelled_payment_failed!
             raise "Cannot mark status as 'cancelled_payment_failed'. Status not 'suspended_payment_failed!" unless suspended_payment_failed?
             self.update_attributes(
@@ -94,14 +94,14 @@ module Subscriptions
             ownerable.cancel_outstanding_invoices!
             status_changed_to_cancelled_payment_failed
           end
-        
+
           def trialing!
             self.update_attributes(
               status: :trialing,
             )
             status_changed_to_trialing
           end
-        
+
           def trial_expired!
             self.update_attributes(
               status: :trial_expired,
@@ -127,8 +127,8 @@ module Subscriptions
               end
             end
           end
-          
-          
+
+
         end
 
         def current_subscription_period
@@ -150,7 +150,7 @@ module Subscriptions
             Rails.logger.debug "Not time to bill Subscription #{id}!"
             return
           end
-            
+
           raise "Subscription #{id} was supposed to bill over 14 days ago! Has something gone wrong?" unless next_bill_date > 14.days.ago
 
           # Handle cancel_at_end
@@ -165,7 +165,7 @@ module Subscriptions
             self.save
             return
           end
-    
+
           # Handle trialing
           if trialing?
             if subscription_periods.size == 0
@@ -183,7 +183,7 @@ module Subscriptions
               return
             end
           end
-    
+
           raise "Subscription #{id} isn't in good standing. It's in #{status}. Can't cycle." unless good_standing? || trialing?
 
           raise "Couldn't find owner for subscription #{id}" if ownerable.nil?
@@ -211,9 +211,9 @@ module Subscriptions
               amount_cents: amount_cents_next_period
             }
           )
-          
+
           next_subscription_period = prepare_next_subscription_period(next_subscription_period, last_subscription_period)
-          
+
           next_subscription_period.save
 
           # Update the subscription for this upcoming month
@@ -250,7 +250,7 @@ module Subscriptions
           # Create a new open invoice for charges that come up between now and next month
           Rails.logger.debug( "subscription#cycle_billing_period!  Creating new open invoice" )
           ownerable.invoices.create( { status: :open } )
-          
+
           if charge_synchronously
             begin
               open_invoice.ready_for_payment!
@@ -278,7 +278,7 @@ module Subscriptions
             "mo"
           end
         end
-        
+
         def interval_to_duration
           case interval
           when "year"
@@ -291,12 +291,12 @@ module Subscriptions
             1.month
           end
         end
-        
+
         def change_plan_to_template!( new_subscription_template, cycle_billing_period_synchronously = false)
           Rails.logger.debug "change_plan_to_template!"
           if trialing? || trial_expired? || cancelled? || cancelled_payment_failed?
-          
-            # If it's cancelled or trial_expired then just change to the new 
+
+            # If it's cancelled or trial_expired then just change to the new
             # template, reset bill date, and cycle.
             # If trialing, don't cycle
             was_trialing = trialing?
@@ -310,40 +310,40 @@ module Subscriptions
                 cycle_billing_period!(cycle_billing_period_synchronously)
               end
             end
-            
+
             return self
           end
-          
+
           # If they're at cancel_at_end we're assuming they're trying to reinstate their subscription by upgrading
           self.uncancel_at_end! if cancel_at_end?
-          
-          
-          
+
+
+
           if subscription_template.present? && subscription_template == new_subscription_template
             Rails.logger.debug "Nothing to change!"
             errors.add(:interval, "That's the plan you're currently on!")
             return false
           end
-          
-          
-          
-          
+
+
+
+
           if new_subscription_template.value_is_equal_to(subscription_template)
             # I'm changing to something in the same template group. Effectively
             # I'm just changing my interval. apply at the end of the period.
             Rails.logger.debug "Matching Group - Changing interval only!"
             assign_mapped_fields_for_template(new_subscription_template)
             self.save
-            
+
           else # I'm changing to a new template value
-          
+
             # New Plan / value
             if new_subscription_template.interval_to_duration < subscription_template.interval_to_duration
               Rails.logger.debug "Cannot change to a shorter interval"
               errors.add(:interval, "You cannot change to a shorter interval")
               return false
             end
-            
+
             if new_subscription_template.value_is_greater_than(subscription_template)
               # Upgrade
               Rails.logger.debug "Upgrade detected"
@@ -353,10 +353,10 @@ module Subscriptions
               perform_prorating(new_subscription_template)
               reset_next_bill_date
               self.save
-              
+
               # We need to cycle the billing period now
               cycle_billing_period!(cycle_billing_period_synchronously)
-              
+
               return self
             else
               # Downgrade
@@ -367,16 +367,16 @@ module Subscriptions
           end
           return self
         end
-        
+
         def value_cents_remaining_on_current_period
           return  0 unless active?
           if current_subscription_period.present?
             start_date = current_subscription_period.start_at
             end_date = self.next_bill_date
-            
+
             total_days_in_period = ((end_date - start_date) / (24 * 60 * 60)).round
             days_left_in_period = ((end_date - Time.now) / (24 * 60 * 60)).round
-            
+
             value_cents_remaining = (self.amount_cents_base * (days_left_in_period.to_f/total_days_in_period)).round
           else
             raise "No current subscription period found for subscription #{self.id}"
@@ -385,9 +385,9 @@ module Subscriptions
 
         def subscription_template
           # Note: If you add custom fields to the subscription templates model, you will want to override this method to check for those as well
-          
+
           # This attempts to find a public subscription template that matches the current subscription settings.
-          
+
           #TODO: Use the mapped fields to do this lookup
           Subscriptions::SubscriptionTemplate.visible.find_or_initialize_by(interval: Subscriptions::Subscription.intervals[interval], amount_cents: amount_cents_base)
         end
@@ -395,16 +395,16 @@ module Subscriptions
         def active?
           good_standing? || cancel_at_end? || trialing?
         end
-        
+
         def assign_mapped_fields_for_template(template)
           Subscriptions::Subscription.subscription_template_field_map.each do |s, t|
             self.send("#{s}=", template.send(t))
           end
         end
-        
+
         def unsuspend_for_payment_failed!
           return if good_standing? # Nothing to see here!
-          
+
           # What we're doing here is checking to see if their subscription was
           # supposed to renew in the next 7 days (or in the past). If it was, we're resetting their bill date so they don't get billed again right away.
           if self.next_bill_date <= 7.days.from_now
@@ -413,50 +413,50 @@ module Subscriptions
           end
           good_standing!
         end
-        
+
         def update_first_paid_at!(force = false)
           return unless force || self.first_paid_at.nil?
           self.update_attributes(
             first_paid_at: (ownerable.invoices.paid.where("total_paid_cents > 0").order(paid_at: :asc).first.paid_at rescue nil)
           )
         end
-        
+
         ########################
         # Hooks
         ########################
-        
+
         def status_changed_to_good_standing
         end
-        
+
         def status_changed_to_suspended
         end
-        
+
         def status_changed_to_cancelled
         end
-        
+
         def status_changed_to_cancel_at_end
         end
-        
+
         def status_changed_to_suspended_payment_failed
         end
-        
+
         def status_changed_to_cancelled_payment_failed
         end
-        
+
         def status_changed_to_trialing
         end
-        
+
         def status_changed_to_trial_expired
         end
-        
+
         def status_reinstated
           # This is called when the user "uncancel_at_end"s their subscription before the end of the period.
         end
-        
+
         #########################
         private
         #########################
-      
+
         def set_initial_amount_cents
           if trialing?
             self.amount_cents_next_period = 0
@@ -504,29 +504,29 @@ module Subscriptions
         def uniqueness_of_ownerable
           errors.add(:ownerable, "Must be unique") if Subscriptions::Subscription.where(ownerable_type: ownerable_type, ownerable_id: ownerable_id).count > 0
         end
-        
+
         def update_current_status_at
           if self.status_changed?
             self.current_status_at = Time.now
           end
         end
-        
+
         ####################################
         ## Hooks mid-cycle billing period ##
         ####################################
-        
+
         def prepare_next_subscription_period(next_subscription_period, current_subscription_period)
           return next_subscription_period
         end
-        
+
         def prepare_subscription_for_next_subscription_period(next_subscription_period)
           return
         end
-        
+
         #######################################
         ## Hooks mid-change_plan_to_template ##
         #######################################
-        
+
         def perform_prorating(new_subscription_template)
           self.amount_cents_next_period   = new_subscription_template.amount_cents - value_cents_remaining_on_current_period
         end
